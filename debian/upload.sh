cbfd36ae59e5e3d5a0d8ff6e4c445a7ac9cbf1fa
Kevin Rosenberg <kevin@rosenberg.net>
2009-08-03 11:19:23 -0600

    fix upload directory

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-dup cl-rss -Ufiles.b9.com -D/home/ftp/xlunit -C"(umask 022; /home/kevin/bin/remove-old-versions cl-rss latest)" -su $*
+dup cl-rss -Ufiles.b9.com -D/home/ftp/cl-rss -C"(umask 022; /home/kevin/bin/remove-old-versions cl-rss latest)" -su $*
