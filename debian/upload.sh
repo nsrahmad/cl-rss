c34b879f8eea24628842b8244615e1dba690ea75
Kevin M. Rosenberg <kevin@rosenberg.net>
2012-09-04 17:20:10 -0600

    Automated commit for debian release 0.9.0-2

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 
-dup cl-rss -Ufiles.b9.com -D/home/ftp/cl-rss -C"(umask 022; /home/kevin/bin/remove-old-versions cl-rss latest)" -su $*
+dup cl-rss -Ufiles.b9.com -D/home/ftp/cl-rss -su \
+    -C"(umask 022; cd /srv/www/html/cl-rss; make install; find . -type d |xargs chmod go+rx; find . -type f | xargs chmod go+r)" $*
