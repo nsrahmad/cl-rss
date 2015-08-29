2aeeb0301ac27d209bf1ee1be120ad0772838b79
Kevin M. Rosenberg <kevin@rosenberg.net>
2015-08-29 00:15:57 -0600

    Update domain name to kpe.io

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-dup cl-rss -Ufiles.b9.com -D/home/ftp/cl-rss -su \
+dup cl-rss -Ufiles.kpe.io -D/home/ftp/cl-rss -su \
     -C"(umask 022; cd /srv/www/html/cl-rss; make install; find . -type d |xargs chmod go+rx; find . -type f | xargs chmod go+r)" $*
